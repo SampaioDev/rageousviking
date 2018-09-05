8e33c0729c9a60da9cc5d04bffe9b51a16680995
davi trigueiro sampaio <1615433>
2018-09-05 16:46:02 -0300

    movimento com setas

--- scene/game.lua
+++ scene/game.lua
@@ -10,6 +10,14 @@ local composer = require("composer")
 
 local scene = composer.newScene()
 
+local physics = require("physics")
+physics.start()
+
+ _W = display.contentWidth; -- Get the width of the screen
+ _H = display.contentHeight; -- Get the height of the screen
+ motionx = 0; -- Variable used to move character along x axis
+ speed = 10; -- Set Walking Speed
+
 function scene:create( event )
 	
 	local count = 0
@@ -20,33 +28,39 @@ function scene:create( event )
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 	
-	local viking = display.newImageRect( "scene/game/img/viking.png", 280, 350 )
+	local viking = display.newImageRect( "scene/game/img/viking.png", 220, 280)
 	viking.x = display.contentCenterX
-	viking.y = display.contentCenterY + 80
+	viking.y = display.contentCenterY + 100
+
+	local left = display.newImageRect( "scene/game/img/arrowL.png", 200, 280 )
+	left.x = -360; left.y = 900;
+
+	local right = display.newImageRect( "scene/game/img/arrowR.png", 200, 280 )
+	right.x = _W - 900; right.y = 900;
 	
-
-	local function dragShip( event )
-
-		local viking = event.target
-		local phase = event.phase
-
-		if ( "began" == phase ) then
-			display.currentStage:setFocus( viking )
-			
-			viking.touchOffsetX = event.x - viking.x
-
-		elseif ( "moved" == phase ) then
-
-			viking.x = event.x - viking.touchOffsetX
-
-		elseif ( "ended" == phase or "cancelled" == phase ) then
-
-			display.currentStage:setFocus( nil )
-		end
-
-		return true 
+	function left:touch()
+			motionx = -speed;
 	end
-	viking:addEventListener( "touch", dragShip )
+	left:addEventListener("touch",left)
+
+	function right:touch()
+		motionx = speed;
+	end
+	right:addEventListener("touch",right)
+
+	local function movePlayer (event)
+		viking.x = viking.x + motionx;
+	end
+	Runtime:addEventListener("enterFrame", movePlayer)
+
+	local function stop (event)
+		if event.phase =="ended" then
+			motionx = 0;
+		end
+		local X, Y = viking:localToContent( 0, 0 )
+		print( "White square's center position in screen coordinates: ", X, Y )
+	end
+	Runtime:addEventListener("touch", stop )
 end
 
 scene:addEventListener( "create" )
