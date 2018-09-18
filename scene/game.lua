7c654abfe7933a27d1e2a602f7f229923158a272
davi trigueiro sampaio <1615433>
2018-09-17 21:05:23 -0300

    Correcao

--- scene/game.lua
+++ scene/game.lua
@@ -8,13 +8,13 @@
 
 local composer = require("composer")
 local scene = composer.newScene()
+local textScore
 local score = 0
 local timerCount
 local warriorTable = {}
 local physics = require( "physics" )
 physics.start()
 physics.setGravity( 0, 0 )
-physics.start()
 
  _W = display.contentWidth; -- Get the width of the screen
  _H = display.contentHeight; -- Get the height of the screen
@@ -27,41 +27,38 @@ function scene:create( event )
 
 	local message = "Clique no lugar para onde você quer se mover"
 
-	local background = display.newImageRect( "scene/game/img/background.png", 1900, 1050 )
+	local background = display.newImageRect( "scene/game/img/background.png", 2600, 1100 )
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 	
-	-- local sheetOptions = {
-	-- 	width =  196.33, 
-	-- 	height = 200, 
-	-- 	numFrames = 36
-	-- }
-	-- local sheet = graphics.newImageSheet("scene/game/img/BronzeKnight.png", sheetOptions)
-
-	-- local sequences = {
-	-- 	{
-	-- 		name = "attacking",
-	-- 		start = 22,
-	-- 		count = 30,
-	-- 		time = 1400,
-	-- 		loopCount = 0,
+	local sheetOptions = {
+	width =  196.33, 
+	height = 200, 
+	numFrames = 36
+	}
+	local sheet = graphics.newImageSheet("scene/game/img/BronzeKnight.png", sheetOptions)
+	local sequences = {
+		{
+			name = "attacking",
+	 		start = 3,
+	 		count = 25,
+	 		time = 800,
+	 		loopCount = 0,
+			speed = speed - 20
 		
-	-- 	}
-	-- }
+		}
+	}
 	
-	-- local running = display.newSprite(sheet, sequences)
-	-- running.x = display.contentCenterX + 800
-	-- running.y = display.contentCenterY + 130
-	-- running.xScale = 1.2
-	-- running.yScale = 1.2
-	local myText = display.newText( score , 100, 200, native.systemFont, 120 )
-	myText:setFillColor( 1	, 1, 1 )
-	myText.x = display.contentCenterX
-	myText.y = display.contentCenterY - 400
+	textScore = display.newText("Score:" ..score , 100, 200, native.systemFont, 100 )
+	textScore:setFillColor( 1	, 1, 1 )
+	textScore.x = display.contentCenterX
+	textScore.y = display.contentCenterY - 450
 	
 	local viking = display.newImageRect( "scene/game/img/viking.png", 220, 280)
 	viking.x = display.contentCenterX - 820
 	viking.y = display.contentCenterY + 130
+	physics.addBody( viking, "static", { radius=70} )
+	viking.myName = "viking"
 
 	local sword = display.newImageRect( "scene/game/img/sword.png", 200, 220 )
 	sword.x = 1000; 
@@ -93,16 +90,19 @@ function scene:create( event )
 			motionx = 0;
 		end
 		local X, Y = viking:localToContent( 0, 0 )
-		print( "White square's center position in screen coordinates: ", X, Y )
 	end
 	Runtime:addEventListener("touch", stop )
 
 	local function createWarrior()
-		score = score + 1
-		myText.text = score
-		local newWarrior = display.newImageRect("scene/game/img/viking2.png", 200, 240)
+		local newWarrior = display.newSprite(sheet, sequences)
+		newWarrior:play()
+		newWarrior.x = display.contentCenterX + 800
+		newWarrior.y = display.contentCenterY + 130
+		newWarrior.xScale = 1.1
+		newWarrior.yScale = 1.1
 	   	table.insert( warriorTable, newWarrior)
-	   	physics.addBody( newWarrior, "dynamic", { radius=70, bounce=0.8 } )
+	   	physics.addBody( newWarrior, "dynamic", { radius=70} )
+		newWarrior.myName = "newWarrior"
 
 	   	local whereFrom = math.random(1)
 
@@ -113,13 +113,33 @@ function scene:create( event )
 			end
 	end
 
+	sword:addEventListener("tap", createWarrior)
+end
+
 	local function loop()
 		createWarrior()
 	end
 
-	sword:addEventListener("tap", createWarrior)
+local function onCollision( event )
+	score = score + 1
+	textScore.text = "Score: " .. score
+	if ( event.phase == "began" ) then
+		local obj1 = event.object1
+		local obj2 = event.object2
+
+		if ( ( obj1.myName == "viking" and obj2.myName == "newWarrior" ) or
+			 ( obj1.myName == "newWarrior" and obj2.myName == "viking" ) )
+		then
+			if(obj1.myName == "newWarrior") then
+				display.remove(obj1)
+			else
+				display.remove(obj2)
+			end
+		end
+	end
 end
 
+Runtime:addEventListener( "collision", onCollision )
 scene:addEventListener( "create" )
 -- scene:addEventListener( "show" )
 -- scene:addEventListener( "hide" )
